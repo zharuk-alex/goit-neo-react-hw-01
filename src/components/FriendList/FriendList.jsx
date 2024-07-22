cb2df94d3dd98e4be5094f0eed55779f3eb5a4b6
zharuk.alex <zzharuk@gmail.com>
2024-07-22 21:59:34 +0300

    init

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,19 +1,20 @@
 import css from "./FriendList.module.css";
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
-  const statusClass = isOnline ? css.positive : css.negative;
   return (
-    <div className={css.card}>
+    <div className={css["list-item"]}>
       <img className={css.img} src={avatar} alt="Avatar" width="48" />
       <p className={css.title}>{name}</p>
-      <p className={statusClass}>{isOnline ? "Online" : "Offline"}</p>
+      <p className={isOnline ? css.positive : css.negative}>
+        {isOnline ? "Online" : "Offline"}
+      </p>
     </div>
   );
 };
 
 export default ({ friends }) => {
   return (
-    <ul className={css.row}>
+    <ul className={css.list}>
       {friends.map(({ id, avatar, name, isOnline }) => (
         <li key={id}>
           <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
